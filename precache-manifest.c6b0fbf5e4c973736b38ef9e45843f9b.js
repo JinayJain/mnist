d7f1982676e967d54d1cde75e26091360fd75e21
Jinay Jain <jinaybjain@gmail.com>
2020-04-22 10:34:47 -0400

    Updates

---
+++ precache-manifest.c6b0fbf5e4c973736b38ef9e45843f9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fab6cb8b1cc2a84e1b59d7a7e799794",
+    "url": "/mnist/index.html"
+  },
+  {
+    "revision": "88a04cabb4c7dfd70552",
+    "url": "/mnist/static/css/main.2654868b.chunk.css"
+  },
+  {
+    "revision": "0921f4e7ecb8bfc7e29a",
+    "url": "/mnist/static/js/2.db203a53.chunk.js"
+  },
+  {
+    "revision": "203136b256d73b86d9b5f7bd77fb7e0d",
+    "url": "/mnist/static/js/2.db203a53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88a04cabb4c7dfd70552",
+    "url": "/mnist/static/js/main.5983d969.chunk.js"
+  },
+  {
+    "revision": "7d18dabb620213a5d86b",
+    "url": "/mnist/static/js/runtime-main.e677d94d.js"
+  }
+]);
